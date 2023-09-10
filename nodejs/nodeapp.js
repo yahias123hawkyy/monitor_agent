3d0521e27b6fff647145b4cd9122227d9839303d
Yahia <yahia@yahia-IdeaPad-L340-15IRH-Gaming>
2023-09-10 18:23:57 +0200

    more file organiszation done + implementing two web apps + creating the logic for monitoring the running containers + attaching a picture for the log files of the of the monitoring containers

---
+++ nodejs/nodeapp.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
